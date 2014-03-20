f66073c7ad46a60effa38b28b063d24bb72bc2eb
Jake Borg <ebmixed@gmail.com>
2014-03-19 22:38:52 -0700

    Keep track of the tail in the list
    That list was pretty bad. Still working on this one, but the gist of it is that we store the tail node separately from the head node instead of iterating through all of them to the find the last one. That way, we can keep updating tail->next to add to the list

--- main.c
+++ main.c
@@ -14,13 +14,14 @@
 
 FILE *init_lister(const char *name, char source_file_name[], char date[]);
 void quit_scanner(FILE *src_file, Token *list);
-void add_token_to_list(Token *list, Token *new_token);
+void add_token_to_list(Token **head, Token **tail, Token *new_token);
 
 int main(int argc, const char * argv[])
 {
     Token *token;
-    Token *token_list = (Token*)malloc(sizeof(Token*)); //This needs to be implemented as a linked list in scanner.h.
-    char source_name[MAX_FILE_NAME_LENGTH];
+    Token *token_list = NULL; //This needs to be implemented as a linked list in scanner.h.
+    Token *tail = NULL;
+	char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     FILE *source_file = init_lister(argv[1], source_name, date);
     init_scanner(source_file, source_name, date);
@@ -28,7 +29,7 @@ int main(int argc, const char * argv[])
     do
     {
         token = get_token();
-        add_token_to_list(token_list, token);
+        add_token_to_list(&token_list, &tail, token);
         print_token(token);
     }
     while (token->code != 19);//What is the sentinal value that ends this loop?
@@ -38,19 +39,16 @@ int main(int argc, const char * argv[])
     quit_scanner(source_file, token_list);
     return 0;
 }
-void add_token_to_list(Token *list, Token *new_token)
+void add_token_to_list(Token **head, Token **tail, Token *new_token)
 {
     // Add new_token to the list knowing that list is a linked list.
-    
-    // Iterates to the last token in the list (by seeing when token->next == NULL), 
-    // then sets the next to new_tokwn
-    Token* temp;
-    if(list == NULL) { // Prime the list
-		*list = *new_token;
+	if(*head == NULL) {
+		*tail = new_token;
+		*head = *tail;
 	}
 	else {
-        for(temp = list; temp->next != NULL; temp = temp->next) {}
-        temp->next = new_token;
+		(*tail)->next = new_token;
+		*tail = new_token;
 	}
 }
 void quit_scanner(FILE *src_file, Token *list)
