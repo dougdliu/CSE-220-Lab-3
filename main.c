1db975cced02d3616251221be7d35d8c9130ae81
achasse <achasse@asu.edu>
2014-03-21 11:59:15 -0700

    updated add_token_to_list with linked list implementation

--- main.c
+++ main.c
@@ -14,14 +14,14 @@
 
 FILE *init_lister(const char *name, char source_file_name[], char date[]);
 void quit_scanner(FILE *src_file, Token *list);
-void add_token_to_list(Token **head, Token **tail, Token *new_token);
+void add_token_to_list(Token **sPtr, Token *new_token);
 
 int main(int argc, const char * argv[])
 {
     Token *token;
     Token *token_list = NULL; //This needs to be implemented as a linked list in scanner.h.
-    Token *tail = NULL;
-	char source_name[MAX_FILE_NAME_LENGTH];
+    
+    char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     FILE *source_file = init_lister(argv[1], source_name, date);
     init_scanner(source_file, source_name, date);
@@ -29,7 +29,7 @@ int main(int argc, const char * argv[])
     do
     {
         token = get_token();
-        add_token_to_list(&token_list, &tail, token);
+        add_token_to_list(&token_list, token);
         print_token(token);
     }
     while (token->code != 18);//What is the sentinal value that ends this loop?
@@ -39,19 +39,28 @@ int main(int argc, const char * argv[])
     quit_scanner(source_file, token_list);
     return 0;
 }
-void add_token_to_list(Token **head, Token **tail, Token *new_token)
+void add_token_to_list(Token **sPtr, Token *new_token)
 {
     // Add new_token to the list knowing that list is a linked list.
     
     // Iterates to the last token in the list (by seeing when token->next == NULL), 
     // then sets the next to new_tokwn
-	if(*head == NULL) {
-		*tail = new_token;
-		*head = new_token;
+	
+	
+	Token *currentPtr;
+	//if the currentPtr is null, then point it to the new_token coming in
+	if(currentPtr == NULL) {
+		
+		currentPtr = new_token;
+		//point the star_ptr to the new_token this is the beginning of the list
+		(*sPtr) = new_token;
 	}
 	else {
-		(*tail)->next = new_token;
-		*tail = new_token;
+		//if current_ptr is not null, say that the next token is the incoming token
+		currentPtr->next = new_token;
+		//then point current_ptr to the incoming token, this way the previous token will be pointing
+		//to the newest token in the list
+		currentPtr = new_token;
 	}
 }
 void quit_scanner(FILE *src_file, Token *list)
