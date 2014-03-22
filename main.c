554059558e85d4d234bd510bd223972b1456132f
Jake Borg <ebmixed@gmail.com>
2014-03-22 00:18:52 -0700

    Reverted back to tracking the tail
    I like this version better, it looks cleaner :3
    Hope you don't mind

--- main.c
+++ main.c
@@ -14,14 +14,14 @@
 
 FILE *init_lister(const char *name, char source_file_name[], char date[]);
 void quit_scanner(FILE *src_file, Token *list);
-void add_token_to_list(Token **sPtr, Token *new_token);
+void add_token_to_list(Token **head, Token **tail, Token *new_token);
 
 int main(int argc, const char * argv[])
 {
     Token *token;
     Token *token_list = NULL; //This needs to be implemented as a linked list in scanner.h.
-    
-    char source_name[MAX_FILE_NAME_LENGTH];
+    Token *tail = NULL;
+	char source_name[MAX_FILE_NAME_LENGTH];
     char date[DATE_STRING_LENGTH];
     FILE *source_file = init_lister(argv[1], source_name, date);
     init_scanner(source_file, source_name, date);
@@ -29,7 +29,7 @@ int main(int argc, const char * argv[])
     do
     {
         token = get_token();
-        add_token_to_list(&token_list, token);
+        add_token_to_list(&token_list, &tail, token);
         print_token(token);
     }
     while (token->code != 18);//What is the sentinal value that ends this loop?
@@ -39,55 +39,17 @@ int main(int argc, const char * argv[])
     quit_scanner(source_file, token_list);
     return 0;
 }
-void add_token_to_list(Token **sPtr, Token *new_token)
+void add_token_to_list(Token **head, Token **tail, Token *new_token)
 {
     // Add new_token to the list knowing that list is a linked list.
-    
-    // Iterates to the last token in the list (by seeing when token->next == NULL), 
-    // then sets the next to new_tokwn
-	
-	
-	Token *currentPtr;
-	Token *previousPtr;
-	
-	previousPtr = NULL;
-	currentPtr = *sPtr;
-	
-	while(currentPtr != NULL) //added a while loop to interate through the list
-	{
-		previousPtr = currentPtr; //interates the ptr.
-		currentPtr = currentPtr->next; //points to the next node.
-	}
-
-	//inserts a new token if there are no token in the linked list
-	if(previousPtr == NULL) {
-
-		currentPtr = new_token;
-		//point the star_ptr to the new_token this is the beginning of the list
-		(*sPtr) = new_token;
-		(*sPtr)->next = NULL;
+	if(*head == NULL) {
+		*tail = new_token;
+		*head = new_token;
 	}
 	else {
-		//at this point, the currentPtr should be null
-		currentPtr = new_token; //This will add the new_token to the 
-		currentPtr->next = NULL;
+		(*tail)->next = new_token;
+		*tail = new_token;
 	}
-	//if the currentPtr is null, then point it to the new_token coming in
-	//if(*sPtr == NULL) {
-//	
-//		currentPtr = new_token;
-//		//point the star_ptr to the new_token this is the beginning of the list
-//		(*sPtr) = new_token;
-//		(*sPtr)->nxt = NULL;
-//	}
-//	else {
-//		//if current_ptr is not null, say that the next token is the incoming token
-//		currentPtr->next = new_token;
-//		//then point current_ptr to the incoming token, this way the previous token will be pointing
-//		//to the newest token in the list
-//		currentPtr = new_token;
-//		currentPtr->next = NULL;
-//	}
 }
 void quit_scanner(FILE *src_file, Token *list)
 {
@@ -97,14 +59,13 @@ void quit_scanner(FILE *src_file, Token *list)
         temp = list->next;
         
         // Free the token's internally allocated memory for word and/or str_lit based on the token code
-        /*
         if(list->code == IDENTIFIER) {
             free(list->word);
         }
         else if(list->code == STRING && list->type == STRING_LIT) {
             free(list->literal.str_lit);
         }
-        */
+        
         free(list); // Free the node
         list = temp;
     }
