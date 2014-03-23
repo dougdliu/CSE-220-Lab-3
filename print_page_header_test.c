484d4745794633a4b3d27ab37496a70f88cbd287
dougdliu <dougdliu@gmail.com>
2014-03-22 21:27:15 -0700

    Update print_page_header_test.c

--- print_page_header_test.c
+++ print_page_header_test.c
@@ -12,6 +12,11 @@ int main()
 	char test_name2[256] = "TestFile.jpg"; //Invalid file name
 	char date1[256] = "Thu Mar  4 08:51:35 2014"; //Valid date
 	char date2[256] = "Tue Mar4 085135 20"; //Invalid date
+	
+	print_page_header(char test_name1[], char date1[]); //VV
+	print_page_header(char test_name1[], char date2[]); //VI
+	print_page_header(char test_name2[], char date1[]); //IV
+	print_page_header(char test_name2[], char date2[]); //II
 }
 
 static void print_page_header(char source_name[], char date[])
