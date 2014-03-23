b0dd0c73a8d899eb706130d6234214420f104d5c
dougdliu <dougdliu@gmail.com>
2014-03-22 20:49:59 -0700

    Create print_page_header_test.c

---
+++ print_page_header_test.c
@@ -0,0 +1,21 @@
+/*
+ * Function test for print_page_header
+ */
+
+#include "print.h"
+
+int main()
+{
+	char test_name1[256] = "TestFile.pas"; //valid file name
+	char test_name2[256] = "TestFile.jpg"; //Invalid file name
+	char date1[256] = "Thu Mar  4 08:51:35 2014"; //Valid date
+	char date2[256] = "Tue Mar4 085135 20"; //Invalid date
+}
+
+static void print_page_header(char source_name[], char date[])
+{
+    static int page_number = 0;
+
+    putchar(FORM_FEED_CHAR);
+    printf("Page    %d  %s  %s\n\n", ++page_number, source_name, date);
+}
